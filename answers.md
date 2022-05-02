9cb340c4d019ebd8f96153c20da4a18b7564ed61
Keith <98364059+KeithMitchell9@users.noreply.github.com>
2022-05-02 04:31:36 +0000

    Completed Assignment

--- answers.md
+++ answers.md
@@ -1,24 +1,23 @@
 # CMPS 2200 Assignment 5
 ## Answers
 
-**Name:**_________________________
+**Name:** Keith J Mitchell
 
 
 Place all written answers from `assignment-05.md` here for easier grading.
 
 
+- **1a.** Given a $N$ dollars, state a greedy algorithm for producing as few coins as possible that sum to $N$. Please discuss if this algorithm is optimal or not.
+  - A greedy algorithm that yields to fewest coins possible would be one which takes the largest valued coins first. The value input would reduce by each coin taken away while greater than 0 and printing out the result once 0 is attained. This would not be an optimal solution as it is too close and slow as a linear search.
+    
+
+- **1b.** What is the work and span of your algorithm?
+  - The work and span of my algorithm would be O(n) 
 
 
+- **2a.** You realize the greedy algorithm you devised above doesn't work in Fortuito. Give a simple counterexample that shows that the greedy algorithm does not produce the fewest number of coins. Please discuss why greedy algorithm cannot work optionally.
+  - A counter example would be with coins = [1, 6, 8, 9, 10] and the input amount being 14. The algorithm used in question 1a would yield (10, 1, 1, 1, 1) while the optimal solution would be (8, 6).
 
-- **1a.**
-
-
-
-
-
-
-
-- **2b.**
-
-
-
+  
+- **2b.** Use this optimal substructure property to design a dynamic programming algorithm for this problem. If you used top-down or bottom-up memoization to avoid recomputing solutions to subproblems, what is the work and span of your approach?
+  - A dynamic approach would be to iterate through every possible combination of coins and finding the one that results in the least total coins used. In doing so the work and span would be increased to O(n^2) as the algorithm now exponentially searches through coin arrangements. 
--- main.py
+++ main.py
@@ -1,29 +1,59 @@
-
-####### Problem 3 #######
-
 test_cases = [('book', 'back'), ('kookaburra', 'kookybird'), ('elephant', 'relevant'), ('AAAGAATTCA', 'AAATCA')]
 alignments = [('book', 'back'), ('kookaburra', 'kookybird-'), ('relev-ant','-elephant'), ('AAAGAATTCA', 'AAA---T-CA')]
 
 def MED(S, T):
-    # TO DO - modify to account for insertions, deletions and substitutions
-    if (S == ""):
-        return(len(T))
-    elif (T == ""):
-        return(len(S))
+  if (S == "") and (T != ""):
+    return(len(T))
+  elif (T == "") and (S != ""):
+    return(len(S))
+  elif (S == "") and (T == ""):
+    return 0
+  else:
+    if (S[0] == T[0]):
+      return(MED(S[1:], T[1:]))
     else:
-        if (S[0] == T[0]):
-            return(MED(S[1:], T[1:]))
-        else:
-            return(1 + min(MED(S, T[1:]), MED(S[1:], T)))
+      return(1 + min(MED(S, T[1:]), MED(S[1:], T)))
 
 
 def fast_MED(S, T, MED={}):
-    # TODO -  implement memoization
-    pass
+  key = S,T
+  if not T:
+    MED[len(S), 0] = len(S)
+    return MED[len(S), 0]
+  elif not S:
+    MED[0, len(T)] = len(T)
+    return MED[0, len(T)]
+  elif key is in MED:
+    return MED[key]
+  elif (S[0] == T[0]):
+    return(fast_MED(S[1:], T[1:]))
+  else:
+    MED[key] = (1 + min(fast_MED(S, T[1:]), fast_MED(S[1:], T), fast_MED(S[1:], T[1:])))
+    return MED[key]
 
 def fast_align_MED(S, T, MED={}):
-    # TODO - keep track of alignment
-    pass
+  key = S,T
+  if (S, T) in MED:
+    return MED[(S, T)]
+  elif not S and not T:
+    return 0
+  elif not S:
+    MED[0, len(T)] = len(T)
+    return MED[0, len(T)] *'-', T
+  elif not T:
+    MED[len(S),0] = len(S)
+    return S,MED[len(S),0] * '-'
+  elif key is in MED:
+    return MED[key]
+  elif S[0] == T[0]:
+    return fast_MED(S[1:], T[1:])
+  else:
+    first = 1 + fast_MED(S, T[1:])
+    second = 1 + fast_MED(S[1:], T)
+    third = 1 + fast_MED(S[1:], T[1:])
+    minimum = min(first, second, third)
+    MED[(S, T)] = minimum
+    return MED[(S, T)]
 
 def test_MED():
     for S, T in test_cases:
